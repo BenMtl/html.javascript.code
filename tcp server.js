1d4a37e4c10b26d875f3b6e446a33538f3e71788
Ben <andreabenini@users.noreply.github.com>
2019-05-17 18:38:26 +0200

    debugging with hex and ascii

--- tcp server.js
+++ tcp server.js
@@ -1,24 +1,43 @@
 //
 // Usage:  node TCPServer.js
 //
-
 // Import net module.
 var net = require('net');
 
+// HEX DUMP
+function hexdump(Message) {
+    var Result = Message.split('').map(function (c) { return c.charCodeAt(0).toString(16).padStart(2, '0'); }).join(' ');
+    return Result;
+}; /**/
+// ASCII DUMP
+function asciidump(Message) {
+    var Result = Message.split('').map(function (c) {
+        if (c.charCodeAt(0) < 32) {
+            return '??';
+        } else {
+            return ' '+c;
+        }
+    }).join(' ');
+    return Result;
+}; /**/
+
 // Create and return a net.Server object, the function will be invoked when client connect to this server.
 var server = net.createServer(function(client) {
-    console.log('+++ Connect. Address: '+client.localAddress+':'+client.localPort+'.     Client: ' + client.remoteAddress + ':' + client.remotePort);
+    console.log('\n+++ Connect. Address: '+client.localAddress+':'+client.localPort+'.     Client: ' + client.remoteAddress + ':' + client.remotePort);
     client.setEncoding('utf-8');
     client.setTimeout(1000);
 
     // When receive client data.
     client.on('data', function (data) {
         // Print received client data and length.
-        hexDump = data.split('').map(function (c) { return c.charCodeAt(0).toString(16); }).join(' ');
-        console.log('<<< ['+data+']  Size:'+client.bytesRead+' ['+hexDump+']');
+        console.log('<<< Size:'+client.bytesRead+' ['+asciidump(data)+']');
+        console.log('         '+''.padEnd(String(client.bytesRead).length, ' ')+' ['+hexdump(data)+']');
         // Server send data back to client use client net.Socket object.
+        data = 'Re:'+data;
+        console.log('>>> Size:'+data.length+' ['+asciidump(data)+']');
+        console.log('         '+''.padEnd(String(client.bytesRead).length, ' ')+' ['+hexdump(data)+']');
         // client.end('Server received data : '+data+', send back to client data size : ' + client.bytesWritten);
-        client.end('Re:'+data);
+        client.end(data);
     });
 
     // When client send data complete.
